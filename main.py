dd78b1f0c2b502807792786a1f0cffcdc11a1ea2
dylan-berndt <technowaffles46@gmail.com>
2024-07-21 20:54:06 -0500

    Ability to request item lists appended

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+token = open('tokens.txt').read().replace('\n', '')
 
-@app.route('/printify-api', methods=['GET'])
-def proxy():
-    adminToken = open('tokens.txt').read().replace('\n', '')
+
+@app.route('/printify', methods=['GET'])
+def shop():
     url = 'https://api.printify.com/v1/shops.json'
     headers = {
-        'Authorization': f'Bearer {adminToken}'
+        'Authorization': f'Bearer {token}'
+    }
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json()), response.status_code
+
+
+@app.route('/printify/items/<int:shopID>')
+def getItems(shopID):
+    url = f'https://api.printify.com/v1/shops/{shopID}/products.json'
+    headers = {
+        'Authorization': f'Bearer {token}'
     }
     response = requests.get(url, headers=headers)
     return jsonify(response.json()), response.status_code

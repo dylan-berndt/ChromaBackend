da991e74ac84b77d3625a332798db59ca3900814
dylan-berndt <technowaffles46@gmail.com>
2024-07-27 13:43:44 -0500

    Caching implemented, and removed unnecessary shopID argument from api calls

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, request
 import requests
 from flask_cors import CORS
+from flask_caching import Cache
 
 app = Flask(__name__)
 CORS(app)
 
+cache = Cache()
+app.config['CACHE_TYPE'] = 'simple'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 240
+app.config['CACHE_KEY_PREFIX'] = 'chromashop_'
+cache.init_app(app)
+
 token = open('tokens.txt').read().replace('\n', '')
+shopID = 16951213
 
 
 @app.route('/printify', methods=['GET'])
@@ -18,8 +26,8 @@ def shop():
     return jsonify(response.json()), response.status_code
 
 
-@app.route('/printify/items/<int:shopID>')
-def getItems(shopID):
+@app.route('/printify/items/')
+def getItems():
     url = f'https://api.printify.com/v1/shops/{shopID}/products.json'
     headers = {
         'Authorization': f'Bearer {token}'
@@ -28,8 +36,8 @@ def getItems(shopID):
     return jsonify(response.json()), response.status_code
 
 
-@app.route('/printify/item/<int:shopID>/<itemID>')
-def getItem(shopID, itemID):
+@app.route('/printify/item/<itemID>')
+def getItem(itemID):
     url = f'https://api.printify.com/v1/shops/{shopID}/products/{itemID}.json'
     headers = {
         'Authorization': f'Bearer {token}'
